1c02f6f4b0246edad0f5166ca287a22ace216ac3
kalyanrj16_local <kalyan.jaladi@gmail.com>
2023-11-06 16:43:34 +0000

    Enabled app service logging

--- server.js
+++ server.js
@@ -7,5 +7,11 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/api/mortgage', mortgageRoutes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- src/db/index.js
+++ src/db/index.js
@@ -12,14 +12,17 @@ const database = client.database(databaseId);
 
 
 async function getMortgageData() {
-  // const { resources: mortgageData } = await container.items.query("SELECT * FROM c").fetchAll();
-  const querySpec = {
-    query: "SELECT TOP 10 * FROM c"
-  };
-  // const { resources: mortgageData } = await database.items.query(querySpec).fetchAll();
-  const { resources: mortgageData } = await database.items.query("SELECT TOP 10 * FROM c").fetchAll();
-  // console.log(mortgageData);  // log data to console
-  return mortgageData;
+  try {
+    const querySpec = {
+      query: "SELECT TOP 10 * FROM c"
+    };
+    const { resources: mortgageData } = await database.container('c').items.query(querySpec).fetchAll();
+    console.log(mortgageData);  // log data to console
+    return mortgageData;
+  } catch (error) {
+    console.error('Error fetching mortgage data:', error);
+    throw error;  // re-throw the error so it can be handled by your error handling middleware
+  }
 }
 
 module.exports = {
