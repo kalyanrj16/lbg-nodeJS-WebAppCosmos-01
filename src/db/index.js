12ca24f2d1d08d16c0a89cbab30cac8d426443c4
kalyanrj16_local <kalyan.jaladi@gmail.com>
2023-11-06 13:14:30 +0000

    Testing sample epc data on lbg-test branch

--- src/db/index.js
+++ src/db/index.js
@@ -3,14 +3,18 @@ const { CosmosClient } = require('@azure/cosmos');
 const endpoint = process.env.ENDPOINT;
 const key = process.env.KEY;
 const databaseId = "customerDetails";
-const containerId = "ctrMortgage01";
+// const containerId = "ctrMortgage01";
 
 const client = new CosmosClient({ endpoint, key });
 const database = client.database(databaseId);
-const container = database.container(containerId);
+// const container = database.container(containerId);
 
 async function getMortgageData() {
-  const { resources: mortgageData } = await container.items.query("SELECT * FROM c").fetchAll();
+  // const { resources: mortgageData } = await container.items.query("SELECT * FROM c").fetchAll();
+  const querySpec = {
+    query: "SELECT TOP 10 * FROM c"
+  };
+  const { resources: mortgageData } = await database.items.query(querySpec).fetchAll();
   return mortgageData;
 }
 
